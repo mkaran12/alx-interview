0424e406c389034da168546bf2391d5b257d9606
moses karany <mosesjnr46466@gmail.com>
2024-10-03 21:26:21 +0000

    0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,16 +3,20 @@
 
 
 def pascal_triangle(n):
-    """Triangle"""
     if n <= 0:
         return []
-    pas = [[1]]
-    for row_number in range(1, n):
-        row = [1]
-        for j in range(1, row_number):
-            element = pas[row_number - 1][j - 1] + pas[row_number - 1][j]
-            row.append(element)
-        row.append(1)
-        pas.append(row)
 
-    return pas
+    triangle = [[1]]  # First row is always [1]
+    
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        new_row = [1]  # Start with 1
+        
+        # Fill the row based on the previous row
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        
+        new_row.append(1)  # End with 1
+        triangle.append(new_row)
+
+    return triangle
